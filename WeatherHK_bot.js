f9d0b19509c250e23cae86f3c1c468ae698b6e19
Roy <roy@gatecoin.com>
2016-07-10 13:38:15 +0800

    WeatherHK_bot: Added Users and getFeedController
    Added users to identify their setting (like subscribed channels and language).
    Return the raw html data to user based on their input

--- WeatherHK_bot.js
+++ WeatherHK_bot.js
@@ -8,6 +8,33 @@ const RssFetcher = require('./weatherRssFeed.js');
 
  var data = fs.readFileSync("data.json");
  var jsonData = JSON.parse(data);
+ var users = [];
+
+ function AddUser(userID)
+ {
+   users[userID] = {};
+   users[userID].language = 'en';
+ }
+
+function GetUserLanguage(userID){
+  if(users.indexOf(userID) === -1){
+    AddUser(userID);
+  }
+  return users[userID].language;
+}
+
+function GetChannel(topic, language)
+{
+  var channel;
+   jsonData.channels.forEach(function fetch(val) {
+     if(val.topic === topic && val.language === language){
+      channel = val;
+      return
+    }
+  });
+  return channel
+}
+
 function StartPolling(){
  setTimeout(function(){
   jsonData.channels.forEach(function fetch(val) {
@@ -26,6 +53,25 @@ StartPolling();
      }
  }
 
+ class GetFeedController extends TelegramBaseController {
+    /**
+     * @param {Scope} $
+     */
+    handle($) {
+      var isTopicSupport = jsonData.topics.indexOf($.query.topic) > -1
+
+      if(isTopicSupport) {
+        var language = GetUserLanguage($.update.message.from.id);
+        var channel = GetChannel($.query.topic, language);
+        var content = RssFetcher.getFeed(channel)
+        //TODO: Parse the html content into telegram message format
+        $.sendMessage(content);
+      }
+      else {
+        $.sendMessage('Topic "' + $.query.topic + '" doesn\'t exist')
+      }
+    }
+}
 
 class InvalidInputController extends TelegramBaseController {
     handle($) {
@@ -35,4 +81,5 @@ class InvalidInputController extends TelegramBaseController {
 
 tg.router
     .when('/topics', new ListTopicController())
+    .when('/tellme :topic', new GetFeedController())
     .otherwise(new InvalidInputController())
